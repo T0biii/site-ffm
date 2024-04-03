0cb01035128951c28275c57b7181b007044b0049
Tobias <tobias-m99@gmx.de>
2024-04-03 02:14:29 +0200

    Update run-build.sh

--- contrib/actions/run-build.sh
+++ contrib/actions/run-build.sh
@@ -34,9 +34,8 @@ export GLUON_SITEDIR="../"
 export GLUON_TARGET="$1"
 export BUILD_LOG=1
 
-BUILD_THREADS="$(($(nproc) + 1))"
 
 echo "Building Gluon with $BUILD_THREADS threads"
 
 #make update
-make -j$BUILD_THREADS V=s
+make V=s
