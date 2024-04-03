4ed323d85fe620f0f07280bfac81c497a5552f08
Tobias <tobias-m99@gmx.de>
2024-04-03 02:18:35 +0200

    Update run-build.sh

--- contrib/actions/run-build.sh
+++ contrib/actions/run-build.sh
@@ -28,14 +28,11 @@
 set -e
 
 export BROKEN=1
-export GLUON_AUTOREMOVE=1
-export GLUON_DEPRECATED=1
 export GLUON_SITEDIR="../"
 export GLUON_TARGET="$1"
 export BUILD_LOG=1
 
-
-echo "Building Gluon with $BUILD_THREADS threads"
+echo "Building FFMUC Firmware"
 
 #make update
 make V=s
